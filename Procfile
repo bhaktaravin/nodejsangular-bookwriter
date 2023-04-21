2d7e03607e1a61015b3452f4b4cdc2b0ed1d8e4a
bhaktaravin <ravin.bhakta@gmail.com>
2023-04-20 18:46:15 -0700

    adding files for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/BookWriter-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/writer/BookController.java
@@ -0,0 +1,37 @@
+package com.writer;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*" )
+public class BookController {
+
+	@Autowired
+	BookRepository bookRepo;
+	
+	
+	@GetMapping("/allBooks")
+	public List<Book> getAll(){
+		return bookRepo.findAll();
+		
+	}
+	
+	@PostMapping("/addBook")
+	public Book createBook(@RequestBody Book book) {
+		
+		return bookRepo.save(book);
+	}
+	
+	@GetMapping("/allBooks/{bookId}")
+	public Optional<Book> getBookById(String id){
+		return bookRepo.findById(id);
+	}
+}
